5416cfba0bc6f7ef368e8708404358b7c3c2089f
Andreas Wüst <andreas.wuest.freelancer@gmail.com>
2025-08-29 21:40:44 +0200

    test debugging updates

--- test/qml/runTests.sh
+++ test/qml/runTests.sh
@@ -1,10 +1,5 @@
 #/bin/bash
-
 set -x
-
-export QT_SELECT=qt5
 rm -rf *.xml
-
-
-env LC_ALL=de_DE.UTF-8 LC_NUMERIC=de_DE.utf8 QML_XHR_ALLOW_FILE_READ=1 qmltestrunner -platform offscreen -junitxml -o junit.xml
+env TZ=Europe/Berlin LC_ALL=de_DE.UTF-8 LC_NUMERIC=de_DE.utf8 QML_XHR_ALLOW_FILE_READ=1 qmltestrunner -platform offscreen -junitxml -o junit.xml
 
